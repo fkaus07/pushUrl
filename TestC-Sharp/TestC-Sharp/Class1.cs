9d9528956d7d0857b5da2cd2855530bcee6fdda2
fkaus07 <yui_lover@naver.com>
2022-10-11 02:19:27 +0900

    sdf

---
+++ TestC-Sharp/TestC-Sharp/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Class11
+    {
+        static void Main()
+        {
+            Console.WriteLine("Main1");
+        }
+    }
+
